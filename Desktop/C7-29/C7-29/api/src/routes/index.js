67c4580355f553bae2635e370711b14d42ad5a7f
Emanuel Juri <76783198+EmanuelJuri@users.noreply.github.com>
2022-09-23 12:15:43 -0300

    Delete Desktop/C7-29/C7-29 directory

--- Desktop/C7-29/C7-29/api/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const userDefault = require('./userDefault')
-
-const router = express.Router()
-
-router.use('/usersDefault', userDefault)
-
-module.exports = router
