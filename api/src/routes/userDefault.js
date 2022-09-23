a8eaa0750234d147e5b94332dfb6661480889187
villalb4 <villalb4angel@gmail.com>
2022-09-23 16:42:51 -0300

    feat[backend]: add CRUD for user userDefault

--- api/src/routes/userDefault.js
+++ api/src/routes/userDefault.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const userDefaultSchema = require('../models/userDefault')
 
 const router = express.Router()
@@ -14,6 +15,21 @@ router.post('/', (req, res) => {
   const user = userDefaultSchema(req.body)
   user.save()
     .then((data) => res.json(data))
+    .catch((error) => res.json({message: error}))
+})
+
+router.put('/:id', (req, res) => {
+  userDefaultSchema.updateOne(
+    {_id: req.params.id},
+    req.body
+  )
+    .then(() => res.send('datos actualizados correctamente'))
+    .catch((error) => res.json({message: error}))
+})
+
+router.delete('/:id', (req,res) => {
+  userDefaultSchema.findByIdAndDelete(req.params.id)
+    .then(() => res.send('usuario eliminado correctamente'))
     .catch((error) => res.json({message: error}))
 })
 
