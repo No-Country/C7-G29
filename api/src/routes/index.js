8f06ac34e707f478731adcb991971ac0bacfe0de
villalb4 <villalb4angel@gmail.com>
2022-09-25 06:16:18 -0300

    feat[backend]: add route publication

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const userDefault = require('./userDefault')
 const userPhotographer = require('./userPhotographer')
+const publication = require('./publication')
 
 const router = express.Router()
 
 router.use('/usersDefault', userDefault)
 router.use('/usersPhotographer', userPhotographer)
+router.use('/publication', publication)
 
 module.exports = router
