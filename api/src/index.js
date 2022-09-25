bc75a3c10c59b44847188aa2e3e4c6079817ffab
villalb4 <villalb4angel@gmail.com>
2022-09-25 14:41:36 -0300

    update cors

--- api/src/index.js
+++ api/src/index.js
@@ -11,18 +11,24 @@ const port = process.env.PORT || 9000;
 app.use(express.json())
 app.use(morgan('dev'))
 
+// cors
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
 app.use('/api', routes)
-
 
 // Conexion a la base de datos (atlas)
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('successful connection to mongodb atlas'))
   .catch((error) => console.log(error))
-
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}/`)
