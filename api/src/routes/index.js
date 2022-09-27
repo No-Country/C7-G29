5a6afb5869f57dce4843f606f093aaede0425176
Franco <juanfrancorodriguez99@gmail.com>
2022-09-27 04:11:44 -0300

    login advances

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const userDefault = require('./userDefault')
-const userPhotographer = require('./userPhotographer')
-const publication = require('./publication')
+const express = require("express");
+const userDefault = require("./userDefault");
+const userPhotographer = require("./userPhotographer");
+const publication = require("./publication");
+const auth0 = require("./auth0Routes");
 
-const router = express.Router()
+const router = express.Router();
 
-router.use('/usersDefault', userDefault)
-router.use('/usersPhotographer', userPhotographer)
-router.use('/publication', publication)
+router.use("/usersDefault", userDefault);
+router.use("/usersPhotographer", userPhotographer);
+router.use("/publication", publication);
+router.use("/Auth0", auth0);
 
-module.exports = router
+module.exports = router;
--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import photos from "../slices/photosSlice";
 import cart from "../slices/cartSlice";
+import user from "../slices/cartSlice";
 
 export default configureStore({
   reducer: {
     photos,
     cart,
+    user,
   },
 });
