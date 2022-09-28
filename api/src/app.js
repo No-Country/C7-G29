1d3dab7acf10ec7b3aee8fba2638864d0bc817d0
villalb4 <villalb4angel@gmail.com>
2022-09-28 16:51:57 -0300

    test

--- api/src/app.js
+++ api/src/app.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const morgan = require("morgan");
 const routes = require("./routes/index");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 9000;
+// const PORT = process.env.PORT || 9001;
 
 app.use(express.json());
 app.use(morgan("dev"));
 
-// cors
+// headers
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
@@ -24,14 +23,4 @@ app.use((req, res, next) => {
 
 app.use("/api", routes);
 
-console.log(process.env.MONGODB_URI);
-
-// Conexion a la base de datos (atlas)
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("successful connection to mongodb atlas"))
-  .catch((error) => console.log(error));
-
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}/`);
-});
+module.exports = app
