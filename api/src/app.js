c6137e0bf036f4935d5e5d92602ab1fcbc348a3d
villalb4 <villalb4angel@gmail.com>
2022-09-30 15:05:20 -0300

    add endpoints in /

--- api/src/app.js
+++ api/src/app.js
@@ -1,26 +1,23 @@
 const express = require("express");
+const cors = require('cors')
 const morgan = require("morgan");
 const routes = require("./routes/index");
 require("dotenv").config();
 
 const app = express();
-// const PORT = process.env.PORT || 9001;
 
+app.use(cors())
 app.use(express.json());
 app.use(morgan("dev"));
 
-// headers
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-
 app.use("/api", routes);
+
+app.get('/', (req, res) => {
+  res.json({
+    "userDefault" : "http://localhost:9000/api/usersDefault",
+    "userPhotographer" : "http://localhost:9000/api/usersPhotographer",
+    "publications" : "http://localhost:9000/api/publication",
+  })
+})
 
 module.exports = app
