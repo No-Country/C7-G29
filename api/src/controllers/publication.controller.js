0045f44f27c746ab6fa060b181aec8e312aeb545
villalb4 <villalb4angel@gmail.com>
2022-09-30 11:57:59 -0300

    Aadd publication controller

---
+++ api/src/controllers/publication.controller.js
@@ -0,0 +1,80 @@
+const publication = require('../models/publication')
+const userPhotographer = require("../models/userPhotographer");
+
+const allPublications = async(req, res) => {
+  try {
+    const photo = await publication.find().populate("photographer", {
+      _id: 1,
+      name: 1,
+      lastName: 1,
+    });
+    if (photo.length === 0) {
+      return res.status(201).send({ message: "No hay publicaciones creadas" });
+    }
+    return res.status(201).json(photo);
+  } catch (error) {
+    return res.status(400).send({ message: error });
+  }
+}
+
+const newPublication = async(req, res) => {
+  try {
+    const {
+      title,
+      description,
+      url,
+      likes,
+      downloads,
+      price,
+      pay,
+      photographer,
+      challenge,
+    } = req.body;
+
+    const userPhoto = await userPhotographer.findById(photographer);
+
+    const photo = await publication({
+      title,
+      description,
+      url,
+      likes,
+      downloads,
+      price,
+      pay,
+      photographer,
+      challenge,
+      photographer: userPhoto._id,
+    });
+    await photo.save();
+
+    userPhoto.publications = userPhoto.publications.concat(photo._id);
+    await userPhoto.save();
+
+    return res
+      .status(201)
+      .send({ message: "Publicacion creada correctamente" });
+  } catch (error) {
+    return res.status(400).send({ message: error });
+  }
+}
+
+const updatePublicaiton = async(req, res) => {
+  await publication.updateOne({ _id: req.params.id }, req.body);
+  res.send("datos actualizados correctamente");
+}
+
+const deletePublication = async(req, res) => {
+  try {
+    if(await publication.findOne({_id: req.params.id})) {
+      await publication.findByIdAndDelete(req.params.id);
+      res.send("Pulicacion eliminada correctamente");
+    } res.send("La publicacion que desea eliminar no existe")
+  } catch (error) {
+    res
+      .status(404)
+      .json({message: error})
+  }
+}
+
+
+module.exports = {allPublications, newPublication, updatePublicaiton, deletePublication}
