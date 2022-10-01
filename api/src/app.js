a24e90d561493394a4d2d9cedf5c1f7c0b645394
villalb4 <villalb4angel@gmail.com>
2022-10-01 16:53:53 -0300

    Change cors to headers

--- api/src/app.js
+++ api/src/app.js
@@ -6,7 +6,19 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors())
+// app.use(cors())
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 app.use(express.json());
 app.use(morgan("dev"));
 
