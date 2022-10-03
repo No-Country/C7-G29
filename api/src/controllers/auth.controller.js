ca0e75b0e8fdbfc4850deb981e8a00c4fdae5d1b
villalb4 <villalb4angel@gmail.com>
2022-10-02 23:43:22 -0300

    change: add relation with publication

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -1,12 +1,18 @@
-const userDefault = require("../models/userDefault");
-const userPhotographer = require("../models/userPhotographer");
+// const userDefault = require("../models/userDefault");
+// const userPhotographer = require("../models/userPhotographer");
 
+// ------ register ------
 const singIn = async (req, res) => {
-	const userFound = await userDefault.findOne({ email: req.body.email });
-	const userPhotographerFound = await userPhotographer.findOne({ email: req.body.email });
-	if (!userFound && !userPhotographerFound)
-		return res.status(404).json({ message: "Usuario no encontrado" });
-	res.json(userFound || userPhotographerFound);
+	// const userFound = await userDefault.findOne({ email: req.body.email });
+	// const userPhotographerFound = await userPhotographer.findOne({ email: req.body.email });
+	// if (!userFound && !userPhotographerFound)
+	// 	return res.status(404).json({ message: "Usuario no encontrado" });
+	// res.json(userFound || userPhotographerFound);
 };
 
-module.exports = { singIn };
+// ------ login ------
+const singUp = async (req, res) => {
+
+}
+
+module.exports = { singIn, singUp };
