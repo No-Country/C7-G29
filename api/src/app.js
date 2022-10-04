ac2033a6106eb8021fdd22e7fd7f88c61b5e6527
villalb4 <villalb4angel@gmail.com>
2022-10-04 17:42:16 -0300

    refactor: add dotenv

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser")
 const morgan = require("morgan");
 const routes = require("./routes/index");
+require("dotenv").config();
 
 
 // MIDDLEWARES
