0d3dd0d7ed530e67bf6c39f081995c8cfb03cdb8
villalb4 <villalb4angel@gmail.com>
2022-10-05 20:38:15 -0300

    change: update component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'
 import Group from './../../assets/Group.png';
 import './Navbar.css';
-import Notificaciones from './../../assets/Vector.png';
-import Subir from './../../assets/iTETAH.tif_1_.png';
-import Carrito from './../../assets/carrito.png';
-import { useAuth0 } from "@auth0/auth0-react";
+// import Notificaciones from './../../assets/Vector.png';
+// import Subir from './../../assets/iTETAH.tif_1_.png';
+// import Carrito from './../../assets/carrito.png';
+
 
 export default function Navbar() {
-    const { loginWithRedirect } = useAuth0();
-    
+
+    const currentUser = useSelector((state) => state.userLoged.currentUser)
+
+    console.log({currentUser})
+
     return (
-    <div>
-        <div className="navbar-general">
-            <div className="navbar-container-img">
-                <img className="navbar-img" src={Group} />
-            </div>
-            <div className="navbar-container-link">
-                <Link className="navbar-link">Categorias</Link>
-                <Link className="navbar-link" to="/publish"><img className="navbar-icon" src={Subir}/>&nbsp; Subir</Link>
-                <Link className="navbar-link"><img className="navbar-icon" src={Carrito}/>&nbsp; Carrito</Link>
-                <Link className="navbar-link"><img src={Notificaciones}/></Link>
-                <Link className="navbar-link" to="/users">Iniciar sesión</Link>
+        <div>
+            <div className="navbar-general">
+                <Link to="/" className="navbar-container-img">
+                    <img className="navbar-img" src={Group} />
+                </Link>
+                <div className="navbar-container-link">
+                    {/* <Link className="navbar-link">Categorias</Link> */}
+                    <Link className="navbar-link" to="/publish">Subir</Link>
+                    <Link className="navbar-link">Carrito</Link>
+                    {/* <Link className="navbar-link"><img src={Notificaciones}/></Link> */}
+                    {currentUser 
+                        ? 
+                        <Link className="navbar-link" to="/users">Iniciar sesión</Link>
+                        : 
+                        <div className='navbar_divAvatar'><img src={currentUser.avatar} alt="" /></div>
+                    }
+                </div>
             </div>
         </div>
-    </div>
   )
 }
