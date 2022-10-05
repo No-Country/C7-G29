c20fab32e6484d9db3d2ccd4c0233e33b46132cf
villalb4 <villalb4angel@gmail.com>
2022-10-05 19:51:11 -0300

    add: new loged route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,10 +4,12 @@ const publication = require("./publication");
 const challenge = require("./challenges");
 const authUser = require("./authUser");
 const searchId = require('./searchId');
+const {verifyToken} = require('../middlewares/auth.middleware')
+
 
 const router = express.Router();
 
-router.use("/users", users);
+router.use("/users", verifyToken, users);
 router.use("/publication", publication);
 router.use("/challenge", challenge);
 router.use("/auth", authUser);
